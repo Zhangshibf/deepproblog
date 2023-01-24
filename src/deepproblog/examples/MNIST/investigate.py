fcfc1d9d31839842bbe03211d43aa05042b19fdc
zhangshibf <zhangshibf@gmail.com>
2023-01-24 12:45:48 +0100

    a

--- src/deepproblog/examples/MNIST/investigate.py
+++ src/deepproblog/examples/MNIST/investigate.py
@@ -5,10 +5,19 @@ import torchvision.transforms as transforms
 from deepproblog.examples.MNIST.network import MNIST_Net
 from deepproblog.examples.MNIST.data import MNIST_train, MNIST_test, addition
 
-_DATA_ROOT = Path(__file__).parent
-print(str(_DATA_ROOT))
-def test_mnistnet(cnn,dataset):
-    pass
+def test_mnistnet(cnn,test_loader):
+    cnn.eval()
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = cnn(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the test images: %d %%' % (100 * correct / total))
 
 
 if __name__ == "__main__":
@@ -18,12 +27,9 @@ if __name__ == "__main__":
 
     datasets = {"test": torchvision.datasets.MNIST(root="/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/dataset", train=False, download=True, transform=transform)}
     test_loader = torch.utils.data.DataLoader(datasets['test'], batch_size=100, shuffle=True)
-    for a,b in test_loader:
-        print(a.size())
-        print(b)
-        break
+#torch.Size([100, 1, 28, 28]) image
     path = ["/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/snapshot/addition1/mnist_net","/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/snapshot/addition2/mnist_net"]
     for i in path:
         network = MNIST_Net()
         network.load_state_dict(torch.load(i)['model_state_dict'])
-        accuracy,loss,cm = test_mnistnet(network,datasets["test"])
+        accuracy,loss,cm = test_mnistnet(network,test_loader)
