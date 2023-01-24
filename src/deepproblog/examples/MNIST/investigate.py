247926b6cfd6ffb8ab7ce8d30356209c72df7c27
zhangshibf <zhangshibf@gmail.com>
2023-01-24 12:32:35 +0100

    a

--- src/deepproblog/examples/MNIST/investigate.py
+++ src/deepproblog/examples/MNIST/investigate.py
@@ -6,23 +6,19 @@ from deepproblog.examples.MNIST.network import MNIST_Net
 from deepproblog.examples.MNIST.data import MNIST_train, MNIST_test, addition
 
 _DATA_ROOT = Path(__file__).parent
+def test_mnistnet(cnn,dataset):
+    pass
 
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
-)
 
-datasets = {
-    "train": torchvision.datasets.MNIST(
-        root=str(_DATA_ROOT), train=True, download=True, transform=transform
-    ),
-    "test": torchvision.datasets.MNIST(
-        root=str(_DATA_ROOT), train=False, download=True, transform=transform
-    ),
-}
+if __name__ == "__main__":
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
 
-path = ["/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/snapshot/addition1/mnist_net","/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/snapshot/addition2/mnist_net"]
-for i in path:
-    network = MNIST_Net()
-    network.load_state_dict(torch.load(i)['model_state_dict'])
-    print(network)
-    print("done")
+    datasets = {"test": torchvision.datasets.MNIST(root=str(_DATA_ROOT), train=False, download=True, transform=transform)}
+    print(datasets)
+    path = ["/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/snapshot/addition1/mnist_net","/home/CE/zhangshi/mlfornlp/mlnlp/src/deepproblog/examples/MNIST/snapshot/addition2/mnist_net"]
+    for i in path:
+        network = MNIST_Net()
+        network.load_state_dict(torch.load(i)['model_state_dict'])
+        accuracy,loss,cm = test_mnistnet(network,datasets["test"])
